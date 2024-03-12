6e8e4a2a65fe2c68503893e6a242465de378c695
Innocent ACOLATSE <inolatse@gmail.com>
2024-03-12 10:39:35 +0000

    Initial commit

---
+++ src/test/java/com/kamis/Car_Rental_Spring/CarRentalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamis.Car_Rental_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
