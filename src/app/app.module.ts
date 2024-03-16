20be54a811cd873361ef6fae32b7d1f6dcaa4053
Innocent ACOLATSE <inolatse@gmail.com>
2024-03-16 08:36:47 +0000

    register page add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,16 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import {HttpClientModule, provideHttpClient} from '@angular/common/http';
+import {NzHeaderComponent} from "ng-zorro-antd/layout";
+import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NzSpinComponent} from "ng-zorro-antd/spin";
+import {NzFormControlComponent, NzFormDirective} from "ng-zorro-antd/form";
+import {NzInputDirective} from "ng-zorro-antd/input";
 
 registerLocaleData(en);
 
@@ -24,7 +31,18 @@ registerLocaleData(en);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NzHeaderComponent,
+    NzRowDirective,
+    NzButtonComponent,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    NzSpinComponent,
+    NzColDirective,
+    NzFormDirective,
+    NzInputDirective,
+    NzFormControlComponent
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US },
