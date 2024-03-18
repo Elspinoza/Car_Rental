17f752ca24d77a193c604a59b0c5e3f36e2b297c
Innocent ACOLATSE <inolatse@gmail.com>
2024-03-18 14:55:19 +0000

    Adding AuthenticationResponse

---
+++ src/main/java/com/kamis/Car_Rental_Spring/dto/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.kamis.Car_Rental_Spring.dto;
+
+import com.kamis.Car_Rental_Spring.enums.UserRole;
+import lombok.Data;
+
+@Data
+public class AuthenticationResponse {
+
+    private String jwt;
+    private UserRole userRole;
+    private Long userId;
+
+}
