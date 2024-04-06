d3982e7e16a271be4e384ea11502f61d60e2ea4f
Innocent ACOLATSE <inolatse@gmail.com>
2024-04-06 12:47:08 +0000

    display of the car updating

--- src/app/modules/admin/components/update-car/update-car.component.ts
+++ src/app/modules/admin/components/update-car/update-car.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {AdminService} from "../../services/admin.service";
+import {ActivatedRoute} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-update-car',
@@ -6,5 +9,58 @@ import { Component } from '@angular/core';
   styleUrl: './update-car.component.scss'
 })
 export class UpdateCarComponent {
+
+  carId: number = this.activatedRoute.snapshot.params['id'];
+  existingImage: string | null = null;
+
+  updateForm!: FormGroup;
+
+  isSpinning: boolean = false;
+  // @ts-ignore
+  selectedFile: File | null;
+  imagePreview: string | ArrayBuffer | null = null;
+  listOfOption: Array<{ label: string; value: string}> = [];
+  listOfBrands = ["BMW", "AUDI", "FERRARI", "TESLA", "MERCEDES", "VOLVO", "TOYOTA", "HONDA", "FORD", "NISSAN", "HYUNDAI", "LEXUS", "KIA"];
+  listOfType = ["Petrol", "Hybrid", "Diesel", "Electric", "CNG"];
+  listOfTransmission = ["Manuel", "Automatic"];
+  listOfColor = ["Red", "White", "Blue", "Black", "Orange", "Grey", "Silver"];
+
+
+  constructor(
+    private adminService: AdminService,
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder,
+  ) {}
+
+  ngOnInit() {
+    this.getCarById();
+    this.updateForm = this.fb.group({
+      name: [null, Validators.required],
+      brand: [null, Validators.required],
+      type: [null, Validators.required],
+      color: [null, Validators.required],
+      transmission: [null, Validators.required],
+      price: [null, Validators.required],
+      description: [null, Validators.required],
+      year: [null, Validators.required],
+    })
+  }
+
+  getCarById() {
+    this.isSpinning = true;
+    this.adminService.getCarById(this.carId).subscribe((res)=> {
+      this.isSpinning = false;
+      // console.log(res);
+      const carDto = res;
+      this.existingImage = 'data:image/jpeg;base64,' + res.returnedImage;
+      console.log(carDto);
+      console.log(this.existingImage);
+      this.updateForm.patchValue(carDto);
+    });
+  }
+
+  submitForm() {
+
+  }
 
 }
