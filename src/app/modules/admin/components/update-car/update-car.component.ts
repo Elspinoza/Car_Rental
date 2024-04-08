40cde1c8c19617ba723ad20d9bb237ca6c01650a
Innocent ACOLATSE <inolatse@gmail.com>
2024-04-08 14:11:00 +0000

    updating METHOD ADD

--- src/app/modules/admin/components/update-car/update-car.component.ts
+++ src/app/modules/admin/components/update-car/update-car.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import {AdminService} from "../../services/admin.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {NzMessageService} from "ng-zorro-antd/message";
 
 @Component({
   selector: 'app-update-car',
@@ -14,10 +15,10 @@ export class UpdateCarComponent {
   existingImage: string | null = null;
 
   updateForm!: FormGroup;
-
+  imgChanged: boolean = false;
   isSpinning: boolean = false;
   // @ts-ignore
-  selectedFile: File | null;
+  selectedFile: any;
   imagePreview: string | ArrayBuffer | null = null;
   listOfOption: Array<{ label: string; value: string}> = [];
   listOfBrands = ["BMW", "AUDI", "FERRARI", "TESLA", "MERCEDES", "VOLVO", "TOYOTA", "HONDA", "FORD", "NISSAN", "HYUNDAI", "LEXUS", "KIA"];
@@ -30,6 +31,8 @@ export class UpdateCarComponent {
     private adminService: AdminService,
     private activatedRoute: ActivatedRoute,
     private fb: FormBuilder,
+    private router: Router,
+    private message: NzMessageService,
   ) {}
 
   ngOnInit() {
@@ -50,17 +53,66 @@ export class UpdateCarComponent {
     this.isSpinning = true;
     this.adminService.getCarById(this.carId).subscribe((res)=> {
       this.isSpinning = false;
-      // console.log(res);
       const carDto = res;
       this.existingImage = 'data:image/jpeg;base64,' + res.returnedImage;
-      console.log(carDto);
-      console.log(this.existingImage);
+      // console.log(carDto);
+      // console.log(this.existingImage);
       this.updateForm.patchValue(carDto);
     });
   }
 
-  submitForm() {
+  updateCar() {
+    this.isSpinning = true;
 
+    const formData: FormData = new FormData();
+    if (this.imgChanged && this.selectedFile) {
+      formData.append('image', this.selectedFile);
+    }
+
+    // @ts-ignore
+    formData.append('brand', this.updateForm.get('brand').value);
+    // @ts-ignore
+    formData.append('name', this.updateForm.get('name').value);
+    // @ts-ignore
+    formData.append('type', this.updateForm.get('type').value);
+    // @ts-ignore
+    formData.append('color', this.updateForm.get('color').value);
+    // @ts-ignore
+    formData.append('year', this.updateForm.get('year').value);
+    // @ts-ignore
+    formData.append('transmission', this.updateForm.get('transmission').value);
+    // @ts-ignore
+    formData.append('description', this.updateForm.get('description').value);
+    // @ts-ignore
+    formData.append('price', this.updateForm.get('price').value);
+
+    console.log(formData);
+
+    this.adminService.updateCar(this.carId, formData).subscribe((res) => {
+      this.isSpinning = false;
+      this.message.success("Car updated successfully", { nzDuration: 5000 });
+      this.router.navigateByUrl("/admin/dashboard");
+      console.log(res);
+    },error => {
+      this.message.error("Error while updating car", { nzDuration: 5000 });
+    })
+
+
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    this.imgChanged = true;
+    this.existingImage = null;
+    this.previewImage();
+  }
+
+  previewImage() {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    }
+    reader.readAsDataURL(this.selectedFile);
   }
 
 }
