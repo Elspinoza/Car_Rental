b371db5ee09a29d153027233ed40ba9b8f540ae6
Innocent ACOLATSE <inolatse@gmail.com>
2024-04-14 15:00:43 +0000

    creating customer root and customer display for getting car by id

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerDashboardComponent } from './components/customer-dashboard/customer-dashboard.component';
+import {NgZorroImportsModule} from "../../NgZorroImportsModule";
+import { BookCarComponent } from './components/book-car/book-car.component';
 
 
 @NgModule({
   declarations: [
-    CustomerDashboardComponent
+    CustomerDashboardComponent,
+    BookCarComponent
   ],
-  imports: [
-    CommonModule,
-    CustomerRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        CustomerRoutingModule,
+
+        NgZorroImportsModule,
+    ]
 })
 export class CustomerModule { }
--- src/app/modules/customer/services/customer.service.ts
+++ src/app/modules/customer/services/customer.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {StorageService} from "../../../auth/services/storage/storage.service";
+import {Observable} from "rxjs";
+
+const BASIC_URL = ["http://localhost:8080"];
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  createAuthorizationHeader(): HttpHeaders {
+    let authHeaders: HttpHeaders = new HttpHeaders();
+    return authHeaders.set('Authorization', 'Bearer '+StorageService.getToken());
+  }
+
+  getAllCars():Observable<any> {
+    return this.http.get(BASIC_URL+"/api/customer/cars", {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+
+  getCarsById(carId: number):Observable<any> {
+    return this.http.get(BASIC_URL+"/api/customer/car/"+carId, {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+
+
 }
